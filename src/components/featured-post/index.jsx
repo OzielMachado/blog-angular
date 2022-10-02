4ea6967496c196a9a0a69324572bd928638261d2
OzielMachado <oziel-bmachado@educar.rs.gov.br>
2022-10-02 17:45:24 -0300

    últimas alterações

--- src/components/featured-post/index.jsx
+++ src/components/featured-post/index.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import AuthorBadge from "../author-badge";
 
 import "./styles.css";
 
-export default function FeaturedPost() {
+export default function FeaturedPost({ post }) {
     return (
         <div className="featured-post-container">
             <div>
-                <img src="https://www.grupotecnofix.com.br/wp-content/uploads/2020/04/landscape-mountains-sky-4843193.jpg" />
+                <img 
+                    className="featured-post-image" 
+                    src={post?.hero?.data?.attributes?.formats?.medium?.url} 
+                />
             </div>
 
             <div>
-                <p>July 24, 2020</p>
+                <p className="featured-post-date">{post?.date}</p>
+                <Link to="#">
+                    <h2 className="featured-post-title">{post.title}</h2>
+                </Link>
+                <p className="featured-post-description">{post.description}</p>
 
-                <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
-                <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique ex in hic enim quis saepe placeat, exercitationem inventore reprehenderit nihil assumenda nostrum sunt commodi, dolore libero ratione, quo distinctio ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, eveniet ad. Cum repellat aperiam laudantium numquam nam, dolor culpa consequatur, adipisci beatae magnam id. Facilis nihil aperiam veritatis est ullam.
-                </p>
+                <AuthorBadge />
             </div>
         </div>
     );
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import FeaturedPost from "../../components/featured-post";
 
+import PostList from "../../components/post-list";
+
 export default function Home() {
+
+    const [posts, setPosts] = useState();
+
+    async function getPosts(){
+        const response = await fetch(
+            "https://cms-blog-tutorial.herokuapp.com/api/posts?populate=*"
+        );
+
+        const data = await response.json();
+
+        setPosts(data.data);
+    }
+
+    useEffect(() => {
+        getPosts();
+    }, [])
+
     return (
         <div>
-            <FeaturedPost />
+            {posts && posts.length > 0 && <FeaturedPost post={posts[0].attributes} />}
+
+            <PostList posts={posts} />
         </div>
     );
 }
